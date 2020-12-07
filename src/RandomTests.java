1897a44cb8c2b2702cf937f5014d42fe36e1e9b6
Jean Pierre <jean.pierre988@hotmail.com>
2020-12-07 17:42:02 -0300

    Random Class
    Text formatting
    Added an throw average
    Added an count of even odd

--- src/RandomTests.java
+++ src/RandomTests.java
@@ -3,13 +3,28 @@ import java.util.Scanner;
 public class RandomTests {
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.print("Entre com a qntidade de lados do dado: ");
+        System.out.println("=======DICE ROLL=======");
+        System.out.print("Entry with the number of sides: ");
         int diceFaces = sc.nextInt();
-        System.out.println("Quantas vezes o dado será jogado?");
+
+        System.out.print("How many times will be play? ");
         int diceTimes = sc.nextInt();
+
+        int odd = 0;
+        int even = 0;
+        int total = 0;
         for (int i = 1; i <= diceTimes; i++) {
-            System.out.println(i+ "ª jogada = " + dice(diceFaces));
+            int dicePlayed = dice(diceFaces);
+            System.out.println(i+ "ª time = " + dicePlayed );
+            if (dicePlayed % 2 == 0) {
+                odd++;
+            } else {
+                even++;
+            }
+            total += dicePlayed;
         }
+        System.out.println("\nLuck = " + (int)total/diceTimes);
+        System.out.println("Odd times = "+odd + " / Even times = " + even);
     }
 
     public static int dice(int max) {
